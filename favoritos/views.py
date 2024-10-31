d7833c4094cd5f15021ced17c75aea8fbe11963b
FelipeGo16 <felipegomezing@gmail.com>
2024-10-31 17:15:54 -0500

    Desarrollo Backend

---
+++ favoritos/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render, redirect
+from .models import Favoritos
+from .forms import FavoritoForm, FavoritoModelForm
+
+# Create your views here.
+def index_favoritos(request):
+    favoritos_lista = Favoritos.objects.all()
+    
+    context = {
+        'favoritos_lista':favoritos_lista
+    }
+
+    return render(request, 'favoritos/lista.html', context)
+
+def crear_favoritos(request):
+
+    form = FavoritoModelForm()
+
+    if request.method == "POST":
+        form = FavoritoModelForm(request.POST)
+        if form.is_valid():
+           form.save()
+        else:
+            print(form.errors)
+            
+    context = {
+        'form': form,
+        'titulo': 'Crear favorito'
+    }
+    
+    return render(request, 'favoritos/crear.html', context)
+
+def borrar_favoritos(request, pk):
+    Favoritos.objects.get(pk=pk).delete()
+    return index_favoritos(request)
+
+def detalles_favoritos(request, pk):
+    favorito = Favoritos.objects.get(pk=pk)
+    return render(request, 'favoritos/detalle.html', context={'object':favorito})
+
+def actualizar_favoritos(request, pk):
+    favorito = Favoritos.objects.get(pk=pk)
+
+    form = FavoritoModelForm(instance=favorito)
+
+    if request.method == "POST":
+        form = FavoritoModelForm(request.POST, instance=favorito)
+        if form.is_valid():
+           form.save()
+        else:
+            print(form.errors)
+            
+    context = {
+        'form': form,
+        'titulo': 'Actualizar favorito'
+    }
+    
+    return render(request, 'favoritos/crear.html', context)
